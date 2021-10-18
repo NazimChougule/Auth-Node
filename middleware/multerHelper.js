a02ab625f4159916b53d800d9cee22a6674c8c3b
nazim <nazimc2394@gmail.com>
2021-10-18 09:59:30 +0530

    removed commented code

--- middleware/multerHelper.js
+++ middleware/multerHelper.js
@@ -28,24 +28,3 @@ var uploadFile = multer({ storage: storage, fileFilter: imageFilter }).single(
 module.exports = {
   uploadFile,
 };
-// module.exports = uploadFile;
-
-// handleFileUpload = (req, res, next) => {
-//   let storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//       fs.mkdirsSync(__dirname + '/uploads/images'); // fs.mkdirsSync will create folders if it does not exist
-//       cb(null, __dirname + '/uploads/images');
-//     },
-//     filename: function (req, file, cb) {
-//       cb(null, Date.now() + '-' + file.originalname);
-//     },
-//   });
-
-//   let upload = multer({ storage: storage }).single('photo');
-// upload(req, res, (err) => {
-//   if (err) {
-//     res.send('somthing went wrong');
-//   }
-//   next();
-// });
-// };
